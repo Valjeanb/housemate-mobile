9f16e91130c571989fcdc2188a8eaa8721087e13
Valjean Boynton <valjean@mistermixer.com.au>
2026-07-08 03:51:51 +1000

    Auto-backup (nightly push) 2026-07-08 03:51

--- app.config.js
+++ app.config.js
@@ -3,6 +3,20 @@ const base = require("./app.json");
 
 module.exports = {
   ...base.expo,
+  // SDK 54: expo install --fix requires these config plugins for native builds
+  // (no effect in Expo Go). Couldn't be auto-written into app.json (dynamic config).
+  plugins: [
+    ...(base.expo.plugins ?? []),
+    "@react-native-community/datetimepicker",
+    "expo-asset",
+    "expo-build-properties",
+    "expo-font",
+    "expo-mail-composer",
+    "expo-secure-store",
+    "expo-sqlite",
+    "expo-video",
+    "expo-web-browser",
+  ],
   android: {
     ...base.expo.android,
     package: "com.vibecode.sitterhub",
